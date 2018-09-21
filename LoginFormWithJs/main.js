b0542a16bfe3639cc03a19cb587416264172ba88
biplavadhikary <42610650+biplavadhikary@users.noreply.github.com>
2018-09-21 11:07:36 +0530

    Updated

---
+++ LoginFormWithJs/main.js
@@ -0,0 +1,13 @@
+function factorial(n)
+{
+    var res=1;
+    for(var i=1;i<=num;++i)
+        res*=i;
+
+    return res;
+}
+alert("Hello!!!");
+var name=prompt("What is your name?");
+console.log(name);
+var num=prompt("Enter the number to find factorial");
+console.log(factorial(num));
